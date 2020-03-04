d6a1354d6fe9a5b3c33ef4dbed526cc1a1ebd02e
tony3fk <tony3fk@yahoo.es>
2020-03-05 00:02:50 +0100

    imagenes portfolio

---
+++ portafolio/src/app/services/productos.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+  productos: Producto[] = [];
+
+  constructor(private http: HttpClient) {
+    this.cargarProductos();
+  }
+
+
+  private cargarProductos() {
+
+    this.http.get('https://angular-html-f6743.firebaseio.com/productos_idx.json').subscribe((resp: Producto[]) => {
+
+      // console.log(resp);
+      this.productos = resp;
+      this.cargando = false;
+
+
+    });
+
+  }
+
+  getProducto(id: string) {
+
+    return this.http.get(`https://angular-html-f6743.firebaseio.com/productos/${id}.json`);
+
+
+  }
+
+
+
+}
